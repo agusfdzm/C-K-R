637175b52b2c97e236cd4f073e25a4c5d29647b9
agusfdzm <129664400+agusfdzm@users.noreply.github.com>
2025-09-17 23:21:13 +0200

    Factorial con punteros

--- Ejercicios Bloque 1/factorialPunteros.c
+++ Ejercicios Bloque 1/factorialPunteros.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-void factorial(int n) {
+void factorial(int* n) {
     int i = 1;
     int nFact = 1;
 
-    while (i <= n) {
+    while (i <= *n) { // Lee el valor de numUser
         nFact = nFact * i;
         i++;
     }
-    printf("%d", nFact);
+    
+    *n = nFact;
 }
 
 int main() {
@@ -16,5 +17,7 @@ int main() {
     printf("Introduce un numero: ");
     scanf("%d", &numUser);
 
-    factorial(numUser);
+    factorial(&numUser); // Pasamos la dirección de memoria de numUser
+
+    printf("El factorial es: %d", numUser);
 }
