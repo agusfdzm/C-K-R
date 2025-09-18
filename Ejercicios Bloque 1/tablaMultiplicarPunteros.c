2ced34c49711f9d26d460554b28821e185aaef66
agusfdzm <129664400+agusfdzm@users.noreply.github.com>
2025-09-18 21:43:49 +0200

    Elegir longitud de la tabla (10-20)

--- Ejercicios Bloque 1/tablaMultiplicarPunteros.c
+++ Ejercicios Bloque 1/tablaMultiplicarPunteros.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-void activador() {
-
-}
-
-void mult(int *n) {
+void mult(int *n, int hasta) {
     int i;
 
-    for (i = 1; i <= 10; i++) {
+    for (i = 1; i <= hasta; i++) {
         int mult = *n * i;
         printf("%d * %d = %d\n", *n, i, mult);
     }
@@ -16,12 +12,17 @@ void mult(int *n) {
 int main() {
     int numUser;
     char bitActivador;
+    int limite = 10;
 
     printf("Pulsa 1 si quieres hacer la tabla de multiplicar hasta 20 y 0 si solo quieres hasta 10. ");
     scanf("%d", &bitActivador);
 
+    if (bitActivador == 1) {
+        limite = 20;
+    }
+
     printf("Introduce el numero que quieras: ");
     scanf("%d", &numUser);
 
-    mult(&numUser);
+    mult(&numUser, limite);
 }
