3a2704b630b42e885492a19677507305ed773975
agusfdzm <129664400+agusfdzm@users.noreply.github.com>
2025-09-18 11:33:10 +0200

    Punteros básicos

--- Ejercicios Bloque 1/tablaMultiplicarPunteros.c
+++ Ejercicios Bloque 1/tablaMultiplicarPunteros.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
-void mult(int n) {
+void activador() {
+
+}
+
+void mult(int *n) {
     int i;
 
     for (i = 1; i <= 10; i++) {
-        int mult = n * i;
-        printf("%d * %d = %d\n", n, i, mult);
+        int mult = *n * i;
+        printf("%d * %d = %d\n", *n, i, mult);
     }
 }
 
 int main() {
     int numUser;
+    char bitActivador;
+
+    printf("Pulsa 1 si quieres hacer la tabla de multiplicar hasta 20 y 0 si solo quieres hasta 10. ");
+    scanf("%d", &bitActivador);
 
     printf("Introduce el numero que quieras: ");
     scanf("%d", &numUser);
 
-    mult(numUser);
+    mult(&numUser);
 }
